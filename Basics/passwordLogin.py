47a804d122d916798eb3b847e5218bee9400f4a5
helperstop <akhilsaiml@gmail.com>
2020-05-07 21:03:44 +0530

    added subprocess functionality

--- Basics/passwordLogin.py
+++ Basics/passwordLogin.py
@@ -1,5 +1,5 @@
 PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6','blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt','luggage': '12345'}
-import sys, pyperclip
+import sys, pyperclip,subprocess
 if len(sys.argv) < 2:
     print('Usage: py pw.py [account] - copy account password')
     sys.exit()
@@ -9,4 +9,5 @@ if account in PASSWORDS:
     print('Password for ' + account + ' copied to clipboard.')
 else:
     print('There is no account named ' + account)
-    
+#subprocess.Popen(['start', '1.mp3'], shell=True)
+subprocess.Popen(r'C:\\Windows\\System32\\calc.exe')
